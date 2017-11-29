11e4d8caf046884627c84e404dd090eff16f9e91
Jayantverma <jayantverma1998@gmail.com>
2017-11-29 17:50:13 +0530

    users passwords

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,7 +8,11 @@ class ArticlesController < ApplicationController
 @article= Article.new
 	end
 	def create 
+		
+
+
 		@article = Article.new(article_params)
+		@article.user = User.first
 		if @article.save
 			flash[:success] = "Article was succesfully created"
 			redirect_to article_path(@article)
